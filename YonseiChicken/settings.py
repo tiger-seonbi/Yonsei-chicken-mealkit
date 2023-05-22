a448b35f934862fcb4a0929c5c5677f9e439a7ea
kimtaekyuni <tgkim2003@gmail.com>
2023-05-22 20:08:53 +0900

    rank3

--- YonseiChicken/settings.py
+++ YonseiChicken/settings.py
@@ -128,3 +128,5 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
